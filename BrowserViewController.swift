6575f21f5293def387da77959b22e75ac8664d64
Sara Tavares <saratavares843@gmail.com>
2023-03-25 23:50:56 +0000

    browser behaves like firefox desktop

--- BrowserViewController.swift
+++ BrowserViewController.swift
@@ -2,35 +2,56 @@ import UIKit
 import WebKit
 import SafariServices
 
-class BrowserViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegate {
+class BrowserViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegate, WKUIDelegate {
 
     var webView: WKWebView!
     var urlTextField: UITextField!
     var backButton: UIBarButtonItem!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // Define the URL of the Firefox extension
+    let firefoxExtensionURL = URL(string: "https://addons.mozilla.org/firefox/downloads/latest/top-sites-button/addon-483724-latest.xpi")!
 
+    override func viewDidLoad() {
+                super.viewDidLoad()
+                
+                // Create a WKWebView instance
+                webView = WKWebView(frame: view.bounds)
+                
+                // Set the custom user agent string to make the website believe that the request is coming from a desktop version of Firefox
+                webView.customUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"
+
+                
+                // Set the navigation delegate to self
+                webView.navigationDelegate = self
+                
+                // Add the web view to the view controller's view
+                view.addSubview(webView)
+                
+                // Load the website
+                let url = URL(string: "https://addons.mozilla.org/en-US/firefox/addon/top-sites-button/")!
+                let request = URLRequest(url: url)
+                webView.load(request)
+           
         // Create the URL bar
         let toolbar = UIToolbar()
         toolbar.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(toolbar)
-        
+
         let backButton = UIBarButtonItem(title: "<", style: .plain, target: self, action: #selector(goBack))
         self.backButton = backButton
-        
+
         let urlTextField = UITextField()
         urlTextField.translatesAutoresizingMaskIntoConstraints = false
         urlTextField.borderStyle = .roundedRect
         urlTextField.delegate = self
         self.urlTextField = urlTextField
-        
+
         let urlBar = UIBarButtonItem(customView: urlTextField)
         toolbar.items = [backButton, UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil), urlBar]
-        
+
         let installButton = UIBarButtonItem(title: "Install Extension", style: .plain, target: self, action: #selector(installExtension))
         navigationItem.rightBarButtonItem = installButton
-        
+
         // Add the toolbar to the bottom of the view
         NSLayoutConstraint.activate([
             toolbar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -48,45 +69,17 @@ class BrowserViewController: UIViewController, WKNavigationDelegate, UITextField
         toolbar.items?.append(contentsOf: [spacer, newTabButton])
 
         // Increase the width of the URL text field
-           urlTextField.widthAnchor.constraint(equalToConstant: 350).isActive = true
-          
-        // Create the web view
-        let webView = WKWebView()
+        urlTextField.widthAnchor.constraint(equalToConstant: 350).isActive = true
+
+        // Add the web view constraints
         webView.translatesAutoresizingMaskIntoConstraints = false
-        webView.navigationDelegate = self
-        view.addSubview(webView)
-        self.webView = webView
-        
-        // Add constraints
         NSLayoutConstraint.activate([
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             webView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40),
             webView.bottomAnchor.constraint(equalTo: toolbar.topAnchor)
         ])
-        
-        // Load the initial URL
-  
-
-        let initialUrl = URL(string: "https://addons.mozilla.org/en-US/firefox/addon/top-sites-button/")!
-        webView.load(URLRequest(url: initialUrl))
-        
     }
-    
-    private func addExtension(from url: URL) {
-        let extensionController = self.webView.configuration.userContentController
-        
-        do {
-            let extensionScript = try String(contentsOf: url, encoding: .utf8)
-            let userScript = WKUserScript(source: extensionScript, injectionTime: .atDocumentStart, forMainFrameOnly: false)
-            extensionController.addUserScript(userScript)
-            
-            print("Extension added successfully")
-        } catch {
-            print("Error adding extension: \(error.localizedDescription)")
-        }
-    }
-    
 
     @objc private func installExtension() {
         let urlString = "https://addons.mozilla.org/firefox/downloads/latest/top-sites-button/addon-483724-latest.xpi"
@@ -110,21 +103,23 @@ class BrowserViewController: UIViewController, WKNavigationDelegate, UITextField
                 print("Error moving file: \(error.localizedDescription)")
             }
         }.resume()
-        
     }
-    
-    // MARK: - Actions
-    
-    @objc func goBack() {
+
+    // MARK: - Navigation
+
+    @objc private func goBack() {
         if webView.canGoBack {
             webView.goBack()
         }
     }
-    
-    @objc func newTab() {
-        // Implement new tab functionality here
+
+    @objc private func newTab() {
+        let newViewController = BrowserViewController()
+        navigationController?.pushViewController(newViewController, animated: true)
     }
-    
+
+    // MARK: - URL Text Field
+
     // MARK: - UITextFieldDelegate
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
